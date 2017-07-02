a80cb6c0b2ceaf9655d49efbf1c1265c7f8447e8
kaname87 <kaname.nakanishi@bibo.com.ph>
2017-07-02 20:17:44 +0800

    move handler to Highest component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import SelectedFoods from './SelectedFoods';
 import FoodSearch from './FoodSearch';
+import Client from './Client';
+
+const MATCHING_ITEM_LIMIT = 25;
 
 class App extends Component {
+
   state = {
     selectedFoods: [],
-  }
+    foods: [],
+    showRemoveIcon: false,
+    searchValue: '',
+  };
 
   removeFoodItem = (itemIndex) => {
     const filteredFoods = this.state.selectedFoods.filter(
@@ -19,6 +26,40 @@ class App extends Component {
     this.setState({ selectedFoods: newFoods });
   }
 
+  handleSearchChange = (e) => {
+    const value = e.target.value;
+
+    this.setState({
+      searchValue: value,
+    });
+
+    if (value === '') {
+      this.setState({
+        foods: [],
+        showRemoveIcon: false,
+      });
+    } else {
+      this.setState({
+        showRemoveIcon: true,
+      });
+
+      Client.search(value, (foods) => {
+        this.setState({
+          foods: foods.slice(0, MATCHING_ITEM_LIMIT),
+        });
+      });
+    }
+  };
+
+  handleSearchCancel = () => {
+    this.setState({
+      foods: [],
+      showRemoveIcon: false,
+      searchValue: '',
+    });
+  };
+
+
   render() {
     const { selectedFoods } = this.state;
 
@@ -30,7 +71,12 @@ class App extends Component {
             onFoodClick={this.removeFoodItem}
           />
           <FoodSearch
+            showRemoveIcon={this.state.showRemoveIcon}
+            foods={this.state.foods}
             onFoodClick={this.addFood}
+            searchValue={this.state.searchValue}
+            handleSearchChange={this.handleSearchChange}
+            handleSearchCancel={this.handleSearchCancel}
           />
         </div>
       </div>
--- client/src/FoodSearch.js
+++ client/src/FoodSearch.js
@@ -2,51 +2,12 @@
 import React from 'react';
 import Client from './Client';
 
-const MATCHING_ITEM_LIMIT = 25;
-
 class FoodSearch extends React.Component {
-  state = {
-    foods: [],
-    showRemoveIcon: false,
-    searchValue: '',
-  };
-
-  handleSearchChange = (e) => {
-    const value = e.target.value;
-
-    this.setState({
-      searchValue: value,
-    });
-
-    if (value === '') {
-      this.setState({
-        foods: [],
-        showRemoveIcon: false,
-      });
-    } else {
-      this.setState({
-        showRemoveIcon: true,
-      });
-
-      Client.search(value, (foods) => {
-        this.setState({
-          foods: foods.slice(0, MATCHING_ITEM_LIMIT),
-        });
-      });
-    }
-  };
-
-  handleSearchCancel = () => {
-    this.setState({
-      foods: [],
-      showRemoveIcon: false,
-      searchValue: '',
-    });
-  };
 
   render() {
-    const { showRemoveIcon, foods } = this.state;
-    const removeIconStyle = showRemoveIcon ? {} : { visibility: 'hidden' };
+    const { showRemoveIcon, foods } = this.props;
+    console.log(this.props)
+    const removeIconStyle = this.props.showRemoveIcon ? {} : { visibility: 'hidden' };
 
     const foodRows = foods.map((food, idx) => (
       <tr
@@ -73,14 +34,15 @@ class FoodSearch extends React.Component {
                       className='prompt'
                       type='text'
                       placeholder='Search foods...'
-                      value={this.state.searchValue}
-                      onChange={this.handleSearchChange}
+                      value={this.props.searchValue}
+                      onChange={this.props.handleSearchChange}
                     />
                     <i className='search icon' />
                   </div>
                   <i
                     className='remove icon'
-                    onClick={this.handleSearchCancel}
+                    // onClick={this.handleSearchCancel}
+                    onClick={this.props.handleSearchCancel}
                     style={removeIconStyle}
                   />
                 </div>
