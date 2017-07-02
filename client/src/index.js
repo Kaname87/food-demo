75fc8d22871e04249ce2dbebbb01b09ad5043d6c
kaname87 <kaname.nakanishi@bibo.com.ph>
2017-07-02 19:40:07 +0800

    add client base code

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+// import '../semantic/dist/semantic.min.css'; // cannot use relative path
+import './semantic-ui/semantic.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'), // eslint-disable-line no-undef
+);
